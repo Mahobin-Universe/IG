7fdd0743d152c69978c580f4f10a8a437122cac2
Mahobin Universe <117584284+Mahobin-Universe@users.noreply.github.com>
2023-08-04 00:09:27 +0600

    Add files via upload

---
+++ ig.py
@@ -0,0 +1,11 @@
+import os, sys 
+ try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests') 
+ except:pass 
+ try:os.system('git pull') 
+ except:pass 
+ try:os.system('xdg-open https://youtube.com/@mahobintechnology') 
+ except:pass 
+ try: 
+     __import__("ig58k").login() 
+ except Exception as e: 
+     exit(str(e))
